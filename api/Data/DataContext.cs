4d8633abf7c15df8be697b6e04e4cb26667481bd
Ailia Martin <ailia.vmd@gmail.com>
2021-05-12 12:33:09 -0500

    End of section 2

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using api.Entities;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions options) : base(options)
+        {  
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
